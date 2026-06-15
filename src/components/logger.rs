8b0ad78b7e25d882e5a0315dafce5cacfd685d4f
SpiralP <spiral.palmer@gmail.com>
2026-06-14 17:25:17 -0700

    init

---
+++ src/components/logger.rs
@@ -0,0 +1,48 @@
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+use crate::component::Component;
+
+#[derive(Default)]
+pub struct Logger;
+
+impl Component for Logger {
+    fn name(&self) -> &'static str {
+        "Logger"
+    }
+
+    fn init(&mut self) {
+        let debug = /* cfg!(debug_assertions) */ true;
+        let other_crates = false;
+
+        let level = if debug { "debug" } else { "info" };
+        let my_crate_name = env!("CARGO_PKG_NAME").replace('-', "_");
+
+        let default_directive = if other_crates {
+            level.parse().unwrap()
+        } else {
+            format!("{my_crate_name}={level}").parse().unwrap()
+        };
+        let filter = EnvFilter::builder()
+            .with_default_directive(default_directive)
+            .from_env_lossy();
+
+        if let Err(e) = tracing_subscriber::fmt()
+            .with_env_filter(filter)
+            .with_target(false)
+            .with_thread_ids(false)
+            .with_thread_names(false)
+            .with_ansi(true)
+            .without_time()
+            .try_init()
+        {
+            eprintln!("failed to init tracing subscriber: {e}");
+        }
+
+        info!(
+            "{} v{} init",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION")
+        );
+    }
+}
